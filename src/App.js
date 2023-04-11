fc88b9e7f5d9889eee4f9f08367e4af8d0d6899c
HasukiOfficial <aliyukamilu002@gmail.com>
2023-04-11 08:20:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <section className="bg-[#9900EF] text-white flex flex-col items-center justify-center min-h-screen text-3xl">
+      
+
+    </section>
+  );
+}
+
+export default App;
